23200090b8b68d3552c884dfda2792ee37473082
Petr Stefan <ptr.stef@gmail.com>
2018-09-05 23:00:28 +0200

    More of the index page

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,5 +3,14 @@
 
     $('.sidenav').sidenav();
 
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true,
+      duration: 300
+    });
+    setInterval(function() {
+      $('.carousel').carousel('next');
+    }, 4000);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
