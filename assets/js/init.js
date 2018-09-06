b75d4f7c2191c56ed5851415aeb18d002545a49c
Petr Stefan <ptr.stef@gmail.com>
2018-09-06 23:05:31 +0200

    Fix carousel height and some improvements

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,16 +1,53 @@
-(function($){
-  $(function(){
-
+(function($) {
+  $(function() {
     $('.sidenav').sidenav();
 
-    $('.carousel.carousel-slider').carousel({
+    $('.carousel').carousel({
       fullWidth: true,
       indicators: true,
       duration: 300
     });
-    setInterval(function() {
-      $('.carousel').carousel('next');
-    }, 4000);
 
+    // hack to properly set initial height of the carousel
+    $('.carousel.carousel-slider')
+      .find('.carousel-item img')
+      .load(function() {
+        $('.carousel.carousel-slider').height($(this).height());
+      });
+
+    var carouselAutoplay = function() {
+      return setInterval(function() {
+        $('.carousel').carousel('next');
+      }, 4000);
+    };
+    var autoplayInstance = carouselAutoplay();
+
+    $('.carousel').hover(
+      function() {
+        clearInterval(autoplayInstance);
+      },
+      function() {
+        clearInterval(autoplayInstance);
+        autoplayInstance = carouselAutoplay();
+      }
+    );
+
+    // move next carousel
+    $('.moveNextCarousel').click(function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('next');
+      clearInterval(autoplayInstance);
+      autoplayInstance = carouselAutoplay();
+    });
+
+    // move prev carousel
+    $('.movePrevCarousel').click(function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('prev');
+      clearInterval(autoplayInstance);
+      autoplayInstance = carouselAutoplay();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
